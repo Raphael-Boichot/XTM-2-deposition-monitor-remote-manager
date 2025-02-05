bc2255c0b77623037f90da466848c75473574c4f
Dr. Raphaël BOICHOT <raphael.boichot@gmail.com>
2025-02-05 14:48:22 +0100

    Add files via upload

--- Code/Microbalance_reader_for_GNU_Octave.m
+++ Code/Microbalance_reader_for_GNU_Octave.m
@@ -7,11 +7,11 @@ disp('|Beware, this code is for GNU Octave ONLY !!!             |')
 disp('-----------------------------------------------------------')
 
 %%%%%%%%%%%%%%%%%%%%%%%user parameter%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-density = '3.970';%4 + point digits mandatory, film density in g/cm3
-z_ratio = '0.336';%4 + point digits mandatory, z-ratio in g/cm3
+density = '1.000';%4 + point digits mandatory, film density in g/cm3
+z_ratio = '1.000';%4 + point digits mandatory, z-ratio in g/cm3
 %The z-ratio is a parameter that corrects the frequency change due to acoustic impedance mismatsch between deposited film and crystal
 tooling = '100.0';%4 + point digits mandatory, tooling factor in %, take into account real measurements vs the device value, let to 100% by default
-fast_mode = 0% fast mode deactivates the plot
+fast_mode = 0;% fast mode deactivates the plot
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 pkg load instrument-control
@@ -22,14 +22,16 @@ list = serialportlist;
 valid_port=[];
 for i =1:1:length(list)
     disp(['Testing port COM',num2str(i),'...'])
-    s = serialport(char(list(i)),'BaudRate',9600,'DataBits',8,'Parity','none','Stopbits',1);
+    %the communication protocol details can be set from keys on the front panel, see documentation
+    s = serialport(char(list(i)),'BaudRate',9600,'dataBits',8,'Parity','none','Stopbits',1);
     set(s, 'timeout',0.1);
     write(s, 'H');
-    write(s, char(6));%mandatory terminator
+    write(s, char(6));%mandatory terminator, char(6) is ACK, if something is wrong, the monitor returns NACK + an error code, see documentation
+    %here I rely on Timeout to be sure to get all the character string. It's not optimal but the GNU Octave library is not as practical as the Matlab one
     response=char(read(s, 20));
-    if length(response)>0
-        if response(1:5)=='XTM/2'
-            disp(['Microbalance ' ,response(1:end-1),' detected on port ',char(list(i))])
+    if ~isempty(response)
+        if strcmp(response(1:5),'XTM/2')
+            disp(['Microbalance ' ,response(1:end-1),' detected on port ',char(list(i))])%last char is ACK
             valid_port=char(list(i));
         end
     end
@@ -37,10 +39,10 @@ for i =1:1:length(list)
 end
 
 %initialization procedure
-s = serialport(valid_port,'BaudRate',9600,'DataBits',8,'Parity','none','Stopbits',1);
+s = serialport(valid_port,'BaudRate',9600,'dataBits',8,'Parity','none','Stopbits',1);
 set(s, 'timeout',0.1);
 
-%Set the density
+%Set the density and read it back to be sure it is set correctly
 mot = ['U 3 1 ',density]; %update density for film 1 (default)
 write(s,mot);
 write(s, char(6));%mandatory terminator
@@ -48,9 +50,9 @@ read(s, 20);%flush serial
 write(s,'Q 3 1'); %query density for film 1 (default)
 write(s, char(6));%mandatory terminator
 response=read(s, 20);
-disp(['Density set to: ',char(response(1:end-1)),' g/cm3']);
+disp(['Density set to: ',char(response(1:end-1)),' g/cm3']);%last char is ACK
 
-%Set the z-ratio
+%Set the z-ratio and read it back to be sure it is set correctly
 mot = ['U 4 1 ',z_ratio]; %update z-ratio for film 1 (default)
 write(s,mot);
 write(s, char(6));%mandatory terminator
@@ -58,9 +60,9 @@ read(s, 20);%flush serial
 write(s,'Q 4 1'); %query z-ratio for film 1 (default)
 write(s, char(6));%mandatory terminator
 response=read(s, 20);
-disp(['Z-ratio set to: ',char(response(1:end-1)),' [-]']);
+disp(['Z-ratio set to: ',char(response(1:end-1)),' [-]']);%last char is ACK
 
-%Set the tooling factor
+%Set the tooling factor and read it back to be sure it is set correctly
 mot = ['U 0 1 ',tooling]; %update tooling for film 1 (default)
 write(s,mot);
 write(s, char(6));%mandatory terminator
@@ -68,7 +70,7 @@ read(s, 20);%flush serial
 write(s,'Q 0 1'); %query tooling for film 1 (default)
 write(s, char(6));%mandatory terminator
 response=read(s, 20);
-disp(['Tooling factor set to: ',char(response(1:end-1)),' %']);
+disp(['Tooling factor set to: ',char(response(1:end-1)),' %']);%last char is ACK
 
 %Set time to zero
 write(s, 'R 5');
@@ -85,7 +87,14 @@ read(s, 20);%flush serial
 write(s,'S 5'); %query tooling for film 1 (default)
 write(s, char(6));%mandatory terminator
 response=read(s, 20);
-disp(['Crystal life: ',char(response(1:end-1)),' %']);
+disp(['Crystal life: ',char(response(1:end-1)),' % (0% is new, 100% is dead)']);
+
+%Measure the crystal present frequency
+read(s, 20);%flush serial
+write(s,'S 8'); %query current frequency
+write(s, char(6));%mandatory terminator
+response=read(s, 20);
+disp(['Crystal current frequency: ',char(response(1:end-1)),' Hz']);
 
 disp('Press x to start and stop measurement')
 while (1)
@@ -101,7 +110,7 @@ disp('Shutter opened');
 read(s, 20);%flush serial
 disp('Starting acquisition...')
 tic
-data=[];
+thickness=[];
 time=[];
 counter=0;
 while (1)
@@ -110,22 +119,23 @@ while (1)
     end
     counter=counter+1;
     pause(0.1)%no need to spam the serial, microbalance is not able to spit more than 10 points per second
+    time=[time;toc];
     write(s,'S 2'); %query thickness for film 1 (default)
     write(s, char(6));%mandatory terminator
-    time=[time;toc];
+    %this function is the only way to read the character string fast enough
     response=ReadToTermination(s,char(6));
-    disp(['Thickness: ',char(response(1:end-2)),' µgm']);
-    data=[data;str2double(char(response(1:end-2)))];
+    disp(['Thickness: ',char(response(1:end-2)),' kAngstrom']);
+    thickness=[thickness;str2double(char(response(1:end-2)))];
 
     if fast_mode==0
-        if counter<=100;
-            plot(time,data,'bd')
+        if counter<=200
+            plot(time,1000*thickness,'dk')%fixed plot
         end
-        if counter>100;
-            plot(time(end-100:end),data(end-100:end),'bd')
+        if counter>200
+            plot(time(end-100:end),1000*thickness(end-100:end),'dk')%sliding plot
         end
         xlabel('Time in seconds');
-        ylabel('Thickness in µgm');
+        ylabel('Thickness in Angstrom');
         set(gca, 'fontsize', 16);
         drawnow
     end
@@ -137,5 +147,17 @@ write(s, 'R 1');
 write(s, char(6));%mandatory terminator
 disp('Shutter closed');
 
-save_data=[time,data];
+%save the data
+save_data=[time,thickness];
 save "-ascii" 'microbalance.txt' save_data
+
+%plot the whole session
+close all
+data=load('microbalance.txt');
+time=data(:,1);
+Thickness=1000*data(:,2);
+plot(time,Thickness,'.k');
+xlabel('Time in seconds');
+ylabel('Thickness in Angstrom');
+set(gca, 'fontsize', 16);
+saveas(gcf,'Thickness_vs_time.png');
